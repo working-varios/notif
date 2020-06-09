d34bf8453ef1df85af778692c4a648fbb84cc410
Carlos Romero <soportecarlosromero@gmail.com>
2020-06-09 11:51:24 -0500

    creacion inicial

---
+++ linea-negocio/src/main/java/com/ito/notifico/lineanegocio/ItoNotificoLineanegocioApplication.java
@@ -0,0 +1,13 @@
+package com.ito.notifico.lineanegocio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItoNotificoLineanegocioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItoNotificoLineanegocioApplication.class, args);
+    }
+
+}
---
+++ linea-negocio/src/test/java/com/ito/notifico/lineanegocio/ItoNotificoLineanegocioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ito.notifico.lineanegocio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItoNotificoLineanegocioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
