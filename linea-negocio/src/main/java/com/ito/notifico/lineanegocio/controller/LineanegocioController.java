df58ddd8d7cf2f89602a97647a7b697659a897e9
Carlos Romero <soportecarlosromero@gmail.com>
2020-06-19 07:36:16 -0500

    menor - comentarios

--- linea-negocio/src/main/java/com/ito/notifico/lineanegocio/controller/LineanegocioController.java
+++ linea-negocio/src/main/java/com/ito/notifico/lineanegocio/controller/LineanegocioController.java
@@ -3,7 +3,6 @@ import com.ito.notifico.lineanegocio.exception.ResourceNotFoundException;
 import com.ito.notifico.lineanegocio.model.LineanegocioEntity;
 import com.ito.notifico.lineanegocio.service.ILineanegocioService;
 import com.ito.notifico.lineanegocio.shared.ResultSearchData;
-import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -18,7 +17,6 @@ public class LineanegocioController {
     @Autowired
     private ILineanegocioService service;
 
-  //  @RequestMapping(method = RequestMethod.POST, path = "/create")
     @PostMapping("/create")
     public ResponseEntity<LineanegocioEntity> create(@RequestBody LineanegocioEntity lineanegocio
             , @RequestHeader("Authorization") int usuarioId)
@@ -33,7 +31,6 @@ public class LineanegocioController {
     }
 
     @PutMapping("/update")
-//    @RequestMapping(method = RequestMethod.PUT)
     public ResponseEntity<LineanegocioEntity> update(@RequestBody LineanegocioEntity lineanegocio
             , @RequestHeader("Authorization") int usuarioId
         ) throws ResourceNotFoundException
@@ -48,7 +45,6 @@ public class LineanegocioController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-//    @RequestMapping(method = RequestMethod.DELETE, path = "/deleteById")
     @DeleteMapping("/deleteById")
     public ResponseEntity<Object> delete(@RequestParam(name = "id") int id) throws ResourceNotFoundException {
         service.deleteById(id);
@@ -56,13 +52,11 @@ public class LineanegocioController {
     }
 
     @GetMapping("/findAll")
-//    @RequestMapping(method = RequestMethod.GET, path = "/findAll")
     public ResponseEntity<Iterable<LineanegocioEntity>> findAll() {
         return new ResponseEntity<>(service.findAll(), HttpStatus.OK);
     }
 
     @GetMapping("/search")
-    //@RequestMapping(method = RequestMethod.GET, path = "/search")
     public ResponseEntity<ResultSearchData<LineanegocioEntity>> search(@RequestParam(name = "page", defaultValue = "0") int page,
         @RequestParam(name = "size", defaultValue = "10") int size, @RequestParam(name = "sortBy") String sortBy
             , @RequestParam(name = "sortOrder") String sortOrder
